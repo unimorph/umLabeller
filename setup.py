1c8a2c76399b199847c98e77cfccdbb7d767b896
Khuyagbaatar Batsuren <khuyagbaatar@gmail.com>
2024-04-23 15:26:38 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='umLabeller',
     version='1.0.0',
     py_modules=['umLabeller'],
-    data_files=[('data', ['eng.word.full.230613.r7.tsv'])],
+    package_data={'umLabeller': ['data/eng.word.full.230613.r7.tsv']},
     description='Morphological inspection tool for subword compositions of tokenizers',
     author='Khuyagbaatar Batsuren',
     url='https://github.com/unimorph/umLabeller',
